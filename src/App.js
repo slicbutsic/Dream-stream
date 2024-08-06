fcad463fd294f684de252092a8ce53a081bd8b2e
Lucas Siviglia <siviglialucas@gmail.com>
2024-08-06 13:05:02 +1000

    adding json file with movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-// import './styles.css';
+import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MoviesGrid from './components/MoviesGrid';
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-// import '../styles.css';
+import '../styles.css';
 
 export default function MoviesGrid(){
 
@@ -8,14 +8,28 @@ export default function MoviesGrid(){
 
   useEffect(() => {
 
-    const m = ["a", "b", "c", "d"];
-    setMovies(m);
 
+    fetch("movies.json")
+    .then((response) => response.json())
+    .then((data) => {
+      setMovies(data);
+    });
   }, []);
 
   return (
-    <div>
-      {movies.length}
+    <div class='movies-grid' >
+      {
+        movies.map(movie => (
+          <div key={movie.id} className='movie-card'>
+            <img src={`images/${movie.image}`} alt={movie.title} />
+            <div className='movie-card-info'>
+              <h3 className='movie-card-title'>{movie.title}</h3>
+              <p className='movie-card-genre'>{movie.genre}</p>
+              <p className='movie-card-rating' >{movie.rating}</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
